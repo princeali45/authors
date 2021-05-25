3c737899465989e4cf4a943cb5fd989d05cd4f74
cafe-oss <cafe.jhonalvin12@gmail.com>
2021-05-25 19:36:54 +0800

    First Commit

---
+++ app/Http/Controllers/UserController2.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\User;
+use App\Traits\ApiResponser;
+
+class UserController2 extends Controller
+{
+    use ApiResponser;
+    private $request;
+
+    public function __construct(Request $request){
+        $this->request = $request;
+    }
+
+    public function getUsers(){
+        $users = User::all();
+        return response()->json($users, 200);
+    }
+
+    public function index()
+    {
+        $users = User::all();
+        return $this->successResponse($users);
+    }
+
+    public function add(Request $request ){
+        $rules = [
+        'username' => 'required|max:20',
+        'password' => 'required|max:20',
+        ];
+        $this->validate($request,$rules);
+        $user = User::create($request->all());
+        return $this->successResponse($user, Response::HTTP_CREATED);
+    }
+
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        // $user = User::where('id', $id)->first();    
+        return $this->successResponse($user);    
+        // return $this->errorResponse('User ID is not found', Response::HTTP_NOT_FOUND); 
+    }
+
+    public function update(Request $request,$id)
+    {
+        $rules = [
+        'username' => 'max:20',
+        'password' => 'max:20',
+        ];
+
+        $this->validate($request, $rules);
+        $user = User::findOrFail($id);  
+        if ($user->isClean()) {
+            return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        $user->save();
+        return $this->successResponse($user);
+    }
+
+    public function delete($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+        return $this->successResponse($user);
+        
+    }
+}
